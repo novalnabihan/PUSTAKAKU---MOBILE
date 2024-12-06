90e18b7cf7f2893ecd26b645c4fb454790960d4a
Fayzul <fayzulhaq0708@gmail.com>
2024-12-07 00:34:39 +0800

    chore: add deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,23 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
 
     implementation (libs.accompanist.pager)
+    val nav_version = "2.8.4"
+
+    // Jetpack Compose integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Views/Fragments integration
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    // Feature module support for Fragments
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+
 }
