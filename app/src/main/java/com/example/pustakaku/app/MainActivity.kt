90ec781c389733b23ccce5e0359bd61c050a3c18
Fayzul <fayzulhaq0708@gmail.com>
2024-12-07 00:33:06 +0800

    chore: remove unused import

--- app/src/main/java/com/example/pustakaku/app/MainActivity.kt
+++ app/src/main/java/com/example/pustakaku/app/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.pustakaku.app
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.pustakaku.HomeScreen
-import com.example.pustakaku.SplashScreen
 import com.example.pustakaku.core.navigation.Navigation
 import com.example.pustakaku.ui.theme.PUSTAKAKUTheme
 
@@ -27,19 +17,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    PUSTAKAKUTheme {
-//        Greeting("Android")
-//    }
-//}
