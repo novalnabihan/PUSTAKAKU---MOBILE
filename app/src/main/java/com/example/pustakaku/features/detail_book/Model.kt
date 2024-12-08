060f2fc2c5d8cd7b77de4eb069a831aad6bd3ba0
Fayzul <fayzulhaq0708@gmail.com>
2024-12-08 13:26:58 +0800

    feat: detail book information

---
+++ app/src/main/java/com/example/pustakaku/features/detail_book/Model.kt
@@ -0,0 +1,21 @@
+package com.example.pustakaku.features.detail_book
+
+data class BookDetailModel(
+  val id: String = "",
+  val title: String = "",
+  val author: String = "",
+  val description: String = "",
+  val likes: Int = 0,
+  val rating: Double = 0.0,
+  val views: Int = 0,
+  val chapters: List<ChapterModel> = emptyList()
+)
+
+data class ChapterModel(
+  val id: String = "",
+  val title: String = "",
+  val content: String = "",
+  val nextPage: String? = null,
+  val prevPage: String? = null,
+  val page:Int = 0
+)
