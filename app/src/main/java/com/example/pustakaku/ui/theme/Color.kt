a69ea25abd4608896fd76b8156d4821af6eaf253
Fayzul <fayzulhaq0708@gmail.com>
2024-12-08 13:27:29 +0800

    feat: add shared components

--- app/src/main/java/com/example/pustakaku/ui/theme/Color.kt
+++ app/src/main/java/com/example/pustakaku/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RedBackground = Color(0xFFFFE5E5)
+val RedText = Color(0xFFCC3333)
+
+val BlueBackground = Color(0xFFE5E5FF)
+val BlueText = Color(0xFF3333CC)
+
+val YellowBackground = Color(0xFFFFF9E5)
+val YellowText = Color(0xFFCC9933)
+
+val GreenBackground = Color(0xFFE5FFE5)
+val GreenText = Color(0xFF339966)
+
+val GrayBackground = Color(0xFFE5E5E5)
+val GrayText = Color(0xFF666666)
