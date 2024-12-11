10a7007fbce1fdc6eb090c6ab03d35f948782883
noval <novalnabihan@gmail.com>
2024-12-12 02:23:07 +0800

    fix: perbaiki tampilan splashscreen yang tidak full

--- app/src/main/java/com/example/pustakaku/SplashScreen.kt
+++ app/src/main/java/com/example/pustakaku/SplashScreen.kt
@@ -1,23 +1,12 @@
-package com.example.pustakaku
-
-
 import android.content.Context
-import androidx.compose.runtime.Composable
-
-import androidx.compose.ui.Modifier
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -27,9 +16,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.pustakaku.app.MainActivity
+import com.example.pustakaku.R
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.firebase.auth.FirebaseAuth
-
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController, context: Context) {
@@ -52,7 +42,6 @@ fun SplashScreen(navController: NavController, context: Context) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 4.dp, vertical = 30.dp)
             .background(color = Color(0xfffffbee)),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -61,28 +50,18 @@ fun SplashScreen(navController: NavController, context: Context) {
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo",
             modifier = Modifier
-                .width(178.dp)
-                .height(136.dp),
-            contentScale = ContentScale.FillBounds,
-
+                .fillMaxWidth(0.6f)
+                .aspectRatio(1f),
+            contentScale = ContentScale.Fit
         )
         Spacer(modifier = Modifier.height(20.dp))
         Text(
             text = "PUSTAKAKU",
             style = TextStyle(
                 fontSize = 30.sp,
-//              fontWeight = FontWeight.Black,
                 fontFamily = font
             )
         )
     }
 }
-
-//@Preview
-//@Composable
-//private fun SplashPrev() {
-//    SplashScreen(navController = navController, this@MainActivity)
-//
-//}
-
 
